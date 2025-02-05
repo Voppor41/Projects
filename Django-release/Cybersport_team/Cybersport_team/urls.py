f58427d11c812a94a7f04ba8aa539edaa7ac64a5
Voppor41 <egorselifanov334@yandex.com>
2025-02-05 16:35:42 +0300

    Initial commit

--- Django-release/Cybersport_team/Cybersport_team/urls.py
+++ Django-release/Cybersport_team/Cybersport_team/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Social_network project.
+URL configuration for Cybersport_team project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
